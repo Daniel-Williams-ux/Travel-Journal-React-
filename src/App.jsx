d95559b2aedfd51ca5dd53915609ed94c1f7fe3b
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-10 13:14:56 +0000

    added css to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import './App.css';
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
-    <main>
-      React⚛️ + Vite⚡ + Replit🌀
-    </main>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function Navbar () { 
+  return (
+    <div>
+    <nav>
+            <img src="../images/icon.jpg" className="nav--logo"/><p>my travel journal</p>
+        </nav>
+      </div>
+  )
+}
